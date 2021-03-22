d98bfb8fe4ae620512849b5e5b6e5cee1e159695
DESKTOP-K19MU2P\cheye <cheyennebolders@gmail.com>
2021-03-22 14:27:23 +0100

    De plugin toegevoegd en gezorgd dat het model zichtbaar is en werkt

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>3D moddel</title>
+
+<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
+</head>
+<body>
+
+<model-viewer src="models/kyubio.glb" style="width: 750px; height: 750px; margin: auto;" alt="A 3D model of an astronaut" auto-rotate camera-controls></model-viewer>
+
+</body>
+</html>
