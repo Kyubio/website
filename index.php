354d8ee486dfc33061d528cc9ed8059ec30186c2
Cheyenne Bolders <c.bolders@student.fontys.nl>
2021-03-29 14:16:20 +0200

    mogelijkheid toegveogd om de kleur van de cubes te veranderen met de color picker

--- index.php
+++ index.php
@@ -6,18 +6,38 @@
 		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
 		<style>
-			body {
-				background-color: #377CDB;
+			body 
+			{
+				background-color: #478FF4;
 			}
-
+			.colors
+			{
+				width: 99%;
+				margin: auto;
+				text-align: center;
+			}
+			.colorPicker 
+			{
+				display: inline-block;
+				margin: 0 10px;
+				color: white;
+			}
 		</style>
 	</head>
 
 	<body>
+		<div class="colors">
+			<span class="colorPicker">Main <input id="main-color" type="color" value="#ffffff"></input><br/></span>
+			<span class="colorPicker">Secondary <input id="secondary-color" type="color" value="#000000"></input><br/></span>
+		</div>
+
+		<div id="container"></div>
+	
 		<script type="module">
 
 			import * as THREE from './three/build/three.module.js';
-
+			
+			import { GUI } from './three/examples/jsm/libs/dat.gui.module.js';
 			import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 			import { RoomEnvironment } from './three/examples/jsm/environments/RoomEnvironment.js';
 
@@ -48,15 +68,38 @@
 				scene.background = new THREE.Color( 0x478FF4 ); //background color
 				scene.environment = pmremGenerator.fromScene( environment ).texture;
 
+				//material
+
+				const mainMaterial = new THREE.MeshPhysicalMaterial( {
+					color: 0xffffff, roughness: 0.4
+				} );
+
+				const secondaryMaterial = new THREE.MeshStandardMaterial( {
+					color: 0x000000, roughness: 0.5
+				} );
+
+				const MainInput = document.getElementById( 'main-color' );
+				MainInput.addEventListener( 'input', function () {
+
+					mainMaterial.color.set( this.value );
+
+				} );
+				
+				const SecondaryInput = document.getElementById( 'secondary-color' );
+				SecondaryInput.addEventListener( 'input', function () {
+
+					secondaryMaterial.color.set( this.value );
+
+				} );
+
 				const loader = new GLTFLoader().setPath( 'models/kyubio2/' );
-				loader.load( 'kyubio22.glb', async function ( gltf ) {
-					
-					const axesHelper = new THREE.AxesHelper(10);
-					scene.add(axesHelper);
+				loader.load( 'kyubio23.glb', async function ( gltf ) {
 
 					const model = gltf.scene;
 					model.scale.set( 0.25, 0.25, 0.25 ); //size of the cube
-					model.setRotationFromQuaternion;
+					model.getObjectByName( 'Cube' ).material = mainMaterial;
+					//model.getObjectByName( 'addons' ).material = secondaryMaterial;
+					
 					
 					const animate = function () //animation of the cube
 					{
@@ -82,7 +125,7 @@
 
 				window.addEventListener( 'resize', onWindowResize );
 
-			}
+			} //end function init
 
 			function onWindowResize() {
 
